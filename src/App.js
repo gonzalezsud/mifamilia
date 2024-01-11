62cc5089e7b4dbea5a97df0f7963a76c18b559e1
gonzalezsud <gonzalezsud@yahoo.com>
2024-01-10 19:51:09 -0600

    1951

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-// App.js
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importa BrowserRouter, Routes y Route
-
-
-
-import Menu from './components/menu'; // Asegúrate de que menu.js esté dentro de la carpeta 'components'
-import RegistroUsuario from './components/RegistroUsuario'; // Asegúrate de que RegistroUsuario.js esté dentro de la carpeta 'components'
-import InicioSesionUsuario from './components/InicioSesionUsuario'; // Asegúrate de que InicioSesionUsuario.js esté dentro de la carpeta 'components'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import RegistroUsuario from './components/RegistroUsuario';
+import InicioSesionUsuario from './components/InicioSesionUsuario';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-             <div className="App">
-        <Menu />
-        <div>
-          <Route path="/registro" component={RegistroUsuario} />
-          <Route path="/inicio-sesion" component={InicioSesionUsuario} />
-        </div>
-      </div>
+        <Route path="/" element={<div className="App">
+          <Menu />
+          <div>
+            <Route path="/registro" element={<RegistroUsuario />} />
+            <Route path="/inicio-sesion" element={<InicioSesionUsuario />} />
+          </div>
+        </div>} />
       </Routes>
- 
-    </Router>
-    
+    </BrowserRouter>
   );
 }
 
 export default App;
-
